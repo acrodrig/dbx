8c3b960893c1502f1411b3d71aded4ebab698939
Andres C. Rodriguez <acrodrig@users.noreply.github.com>
2022-05-27 21:37:21 -0700

    Upgrading dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
 export { default as EventEmitter } from "https://deno.land/x/events@v1.0.0/mod.ts";
 
-import * as log from "https://deno.land/std@0.129.0/log/mod.ts";
-import { LogLevels } from "https://deno.land/std@0.129.0/log/mod.ts";
+import * as log from "https://deno.land/std@0.141.0/log/mod.ts";
+import { LogLevels } from "https://deno.land/std@0.141.0/log/mod.ts";
 
 const DEBUG = Deno.env.get("DEBUG") ?? "";
 const CONSOLE = new log.handlers.ConsoleHandler("DEBUG");
--- test/deps.ts
+++ test/deps.ts
@@ -1,2 +1,2 @@
-export * as path from "https://deno.land/std@0.132.0/path/mod.ts";
+export * as path from "https://deno.land/std@0.141.0/path/mod.ts";
 export { assert } from "https://cdn.skypack.dev/chai";
